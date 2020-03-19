4984c756c412c52b9eb95ddc1b52bed5782e3c30
huangyouhua <1356795413@qq.com>
2020-03-19 21:54:44 +0800

    third commit

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = "/home/hyh/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
+whitelisted_packages = "plugin_calculator".split(';') if "plugin_calculator" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
 underlay_workspaces = "/home/hyh/catkin_ws/devel;/home/hyh/mc_ws/devel;/opt/ros/kinetic".split(';') if "/home/hyh/catkin_ws/devel;/home/hyh/mc_ws/devel;/opt/ros/kinetic" != "" else []
